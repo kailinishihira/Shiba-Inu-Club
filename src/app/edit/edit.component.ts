90a0f8402142812effa19769c9e6c513f59f8651
Kaili Nishihira <kailinishihira@gmail.com>
2017-09-29 13:04:19 -0700

    add full crud functionality

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Member } from '../member.model';
+import { MemberService } from '../member.service';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
-  styleUrls: ['./edit.component.css']
+  styleUrls: ['./edit.component.css'],
+  providers: [MemberService]
 })
 export class EditComponent implements OnInit {
+  @Input() selectedMember;
 
-  constructor() { }
+  constructor(private memberService: MemberService) { }
 
   ngOnInit() {
   }
 
+  updateThisMember(memberToUpdate){
+    this.memberService.updateMember(memberToUpdate);
+  }
+
+  deleteThisMember(memberToDelete){
+    if(confirm("Are you sure you want to delete this member?")) {
+      this.memberService.deleteMember(memberToDelete);
+    }
+  }
 }
